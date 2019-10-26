3d410a27b1a63038eb413d748d7838573608d28c
Tanya <txazari@gmail.com>
2019-10-25 20:06:38 -0700

    Completed Part 2 of Flix Assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by t on 10/25/19.
+//  Copyright © 2019 TanyaAzari. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
